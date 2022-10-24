b82fc8326acdfee707dffb37dc63a2f239b31bff
danieliniguezv <48453683+danieliniguezv@users.noreply.github.com>
2022-10-23 20:34:03 -0500

    Adding homework answers to contract

--- week-1/homework-3/Bootcamp.sol
+++ week-1/homework-3/Bootcamp.sol
@@ -6,6 +6,15 @@ contract BootcampContract {
 	
 	uint256 number;
 
+	address owner;
+
+	address foreign;
+
+	constructor {
+		owner = msg.sender;
+		foreign = 0x000000000000000000000000000000000000dEaD;
+	}
+
 	function store(uint256 num) public {
 		number = num;
 	}
@@ -13,4 +22,12 @@ contract BootcampContract {
 	function retreive() public view returns (uint256) {
 		return number;
 	}
+
+	function deployer() external view returns (address) {
+		if (msg.sender == owner) {
+			return foreign;
+		}
+		else {
+			return owner;
+		}
 }
