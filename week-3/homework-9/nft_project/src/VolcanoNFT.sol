c31c0165d72f93f0220ee7362471d09934fa5618
Daniel <daniel.iniguez.villa@gmail.com>
2022-11-11 23:10:18 +0000

    Added minting function to contract

--- week-3/homework-9/nft_project/src/VolcanoNFT.sol
+++ week-3/homework-9/nft_project/src/VolcanoNFT.sol
@@ -6,4 +6,9 @@ import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 contract VolcanoNFT is ERC721 {
     
+    constructor() ERC721("Volcano NFT", "VLC"){}
+
+    function mint(address _to, uint256 _tokenId) external {
+        super._safeMint(_to, _tokenId);
+    } 
 }
