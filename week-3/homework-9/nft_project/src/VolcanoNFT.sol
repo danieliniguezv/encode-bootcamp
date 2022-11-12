78511113929885e5d1a5e49ff7afa198f8c499b4
Daniel <daniel.iniguez.villa@gmail.com>
2022-11-12 20:37:26 +0000

    Added transfer function

--- week-3/homework-9/nft_project/src/VolcanoNFT.sol
+++ week-3/homework-9/nft_project/src/VolcanoNFT.sol
@@ -5,10 +5,21 @@ pragma solidity ^0.8.0;
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 contract VolcanoNFT is ERC721 {
+
+    //Mapping the token ID with the actual NFT.
+    mapping(uint256 => string) public nft;
     
+    //Pass in the name of the NFT and its symbol.
     constructor() ERC721("Volcano NFT", "VLC"){}
 
-    function mint(address _to, uint256 _tokenId) external {
-        super._safeMint(_to, _tokenId);
-    } 
+    //This NFT stores a string.
+    function mint(uint256 _tokenId, string memory _nft) external {
+        super._safeMint(msg.sender, _tokenId);
+        nft[_tokenId] = _nft;
+    }
+
+    //Transfer the NFT to a given addres.
+    function transfer(address from_, address to_, uint256 tokenId_) external {
+        super.safeTransferFrom(from_, to_, tokenId_);
+    }
 }
