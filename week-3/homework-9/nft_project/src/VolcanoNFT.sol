1edbf4af0a22e35a6ce7a3e80657c5834dd54380
Daniel <daniel.iniguez.villa@gmail.com>
2022-11-13 01:54:10 +0000

    Added Token ID variable

--- week-3/homework-9/nft_project/src/VolcanoNFT.sol
+++ week-3/homework-9/nft_project/src/VolcanoNFT.sol
@@ -5,12 +5,19 @@ pragma solidity ^0.8.0;
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 contract VolcanoNFT is ERC721 {
+
+    uint256 public tokenId_;
     
     //Mapping the token ID with the actual NFT.
     mapping(uint256 => string) public nft;
     
     //Pass in the name of the NFT and its symbol.
-    constructor(string memory _nftName, string memory _nftSymbol) ERC721(_nftName, _nftSymbol){}
+    constructor(
+        string memory _nftName, 
+        string memory _nftSymbol) 
+        ERC721(_nftName, _nftSymbol){
+            tokenId_ = 0;
+        }
 
     //This NFT stores a string.
     function mint(uint256 _tokenId, string memory _nft) external {
