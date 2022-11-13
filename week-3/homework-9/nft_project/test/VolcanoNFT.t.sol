0f0843bb8142aa922a0acb59c759f88a9c835764
Daniel <daniel.iniguez.villa@gmail.com>
2022-11-13 02:44:22 +0000

    Added transfer unit test

--- week-3/homework-9/nft_project/test/VolcanoNFT.t.sol
+++ week-3/homework-9/nft_project/test/VolcanoNFT.t.sol
@@ -3,19 +3,22 @@
 pragma solidity ^0.8.0;
 
 import "../lib/forge-std/src/Test.sol";
-import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import "../src/VolcanoNFT.sol";
 
-contract VolcanoNFTTest is Test, ERC721 {
+contract VolcanoNFTTest is Test {
 
-    VolcanoNFT public vNFT;
+    VolcanoNFT vNFT;
 
     function setUp() public {
-        vNFT = new VolcanoNFT();
+        vNFT = new VolcanoNFT("Test Volcano NFT", "TVLC");
     }
 
     function testMint() public {
-        vNFT.mint();
-        asserTrue(vNFT._tokenId != 0);
+        vNFT.mint("This is a NFT");
+    }
+
+    function testTransfer() public {
+        vNFT.mint("This is a NFT");
+        vNFT.transfer(address(this), address(this), 1);
     }
 }
