4458bdedeaa7904840f12ab18c115551873ffe7d
Daniel <daniel.iniguez.villa@gmail.com>
2022-11-14 02:26:24 +0000

    Added price to mint NFT and withdrawl function

--- week-3/homework-11/volcano_nft/src/VolcanoNFT.sol
+++ week-3/homework-11/volcano_nft/src/VolcanoNFT.sol
@@ -3,10 +3,12 @@
 pragma solidity ^0.8.0;
 
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract VolcanoNFT is ERC721 {
+contract VolcanoNFT is ERC721, Ownable {
 
     uint256 public _tokenId;
+    uint256 private _price;
     
     //Mapping the token ID with the actual NFT.
     mapping(uint256 => string) public nft;
@@ -17,10 +19,12 @@ contract VolcanoNFT is ERC721 {
         string memory _nftSymbol) 
         ERC721(_nftName, _nftSymbol){
             _tokenId = 0;
+            _price = 0.01 ether;
         }
 
     //This NFT stores a string.
-    function mint(string memory _nft) external {
+    function mint(string memory _nft) external payable {
+        require(msg.value == _price);
         _tokenId++;
         super._mint(msg.sender, _tokenId);
         nft[_tokenId] = _nft;
@@ -29,5 +33,10 @@ contract VolcanoNFT is ERC721 {
     //Transfer the NFT to a given addres.
     function transfer(address from_, address to_, uint256 tokenId_) external {
         super._transfer(from_, to_, tokenId_);
+    }
+
+    //Withdraw NFT sales earnings.
+    function withdrawl(address payable _to) public onlyOwner {
+        _to.transfer(address(this).balance);
     }
 }
