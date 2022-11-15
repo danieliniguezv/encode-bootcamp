7f3141890f3abb241712c3039f45cab3b120f735
Daniel <daniel.iniguez.villa@gmail.com>
2022-11-15 04:23:31 +0000

    Added ETH and Volcano token payments to mint

--- week-3/homework-11/volcano_nft/src/VolcanoNFT.sol
+++ week-3/homework-11/volcano_nft/src/VolcanoNFT.sol
@@ -4,11 +4,16 @@ pragma solidity ^0.8.0;
 
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "../lib/volcano-coin/VolcanoCoin.sol";
 
 contract VolcanoNFT is ERC721, Ownable {
 
+    VolcanoCoin volcanoToken;
+
     uint256 public _tokenId;
     uint256 public _price;
+    uint256 public _volcanoPrice;
+    uint256 public volcanoAmount_;
     
     //Mapping the token ID with the actual NFT.
     mapping(uint256 => string) public nft;
@@ -19,13 +24,26 @@ contract VolcanoNFT is ERC721, Ownable {
         string memory _nftSymbol
         ) 
         ERC721(_nftName, _nftSymbol) {
+            volcanoToken = VolcanoCoin(/*Address for deployed contract*/);
             _tokenId = 0;
             _price = 0.01 ether;
+            _volcanoPrice = 1;
         }
+    
+    //Pay with Volcano token.
+    function payVolcano(uint256 _volcanoAmount) public {
+        volcanoAmount_ = _volcanoAmount;
+        require(_volcanoAmount == _volcanoPrice);
+        volcanoToken.transfer(_volcanoAmount, msg.sender, address(this));
+
+    }
 
     //This NFT stores a string.
     function mint(string memory _nft) external payable {
-        require(msg.value == _price);
+        if (msg.value < _price) {
+            if (volcanoAmount_ < _volcanoPrice) revert("NFT price is 0.01 ETH or 1 VCC");
+        }
+        volcanoAmount_ = 0;
         _tokenId++;
         super._mint(msg.sender, _tokenId);
         nft[_tokenId] = _nft;
@@ -39,5 +57,15 @@ contract VolcanoNFT is ERC721, Ownable {
     //Withdraw NFT sales earnings.
     function withdrawl(address payable _to) public onlyOwner {
         _to.transfer(address(this).balance);
+    }
+
+    //Volcano token contract balance.
+    function getVolcanoContractBalance() public view returns (uint) {
+        return volcanoToken.getUserBalances(address(this));
+    }
+
+    //Volcano token user balance.
+    function getVolcanoUserBalance() public view returns (uint) {
+        return volcanoToken.getUserBalances(msg.sender);
     }
 }
