75d3b6d607a5d2297376181847b518eb78b70cb3
Daniel <daniel.iniguez.villa@gmail.com>
2022-11-22 22:36:17 +0000

    Gas optimization v0.3

--- week-3/homework-12/SolidityBootcamp/Exercises/Gas/contracts/Gas.sol
+++ week-3/homework-12/SolidityBootcamp/Exercises/Gas/contracts/Gas.sol
@@ -4,37 +4,27 @@ pragma solidity ^0.8.0;
 
 contract GasContract {
     uint256 public totalSupply; // cannot be updated
-    address public contractOwner;
+    address contractOwner;
     address[5] public administrators;
-    mapping(address => uint256) public balances;
-    mapping(address => Payment[]) public payments;
+    mapping(address => uint256) balances;
+    mapping(address => Payment[]) payments;
     mapping(address => uint256) public whitelist;
-    
-    enum PaymentType {
-        Unknown,
-        BasicPayment,
-        Refund,
-        Dividend,
-        GroupPayment
-    }
 
     struct Payment {
-        PaymentType paymentType;
+        uint256 paymentType;
         uint256 paymentID;
         uint256 amount;
     }
     
     struct ImportantStruct {
-        uint256 valueA; // max 3 digits
         uint256 bigValue;
-        uint256 valueB; // max 3 digits
     }
 
     modifier onlyAdminOrOwner() {
         if (checkForAdmin(msg.sender)) {
             _;
         } else {
-            revert("Error in Gas contract - onlyAdminOrOwner modifier : revert happened because the originator of the transaction was not the admin, and furthermore he wasn't the owner of the contract, so he cannot run this function");
+            revert();
         }
     }
 
@@ -46,14 +36,11 @@ contract GasContract {
         balances[contractOwner] = totalSupply;
 
         for (uint256 ii = 0; ii < administrators.length; ii++) {
-            if (_admins[ii] != address(0)) {
-                administrators[ii] = _admins[ii];
-            }
+            administrators[ii] = _admins[ii];            
         }
     }
 
-    function checkForAdmin(address _user) public view returns (bool) {
-        bool admin = false;
+    function checkForAdmin(address _user) public view returns (bool admin) {
         for (uint256 ii = 0; ii < administrators.length; ii++) {
             if (administrators[ii] == _user) {
                 admin = true;
@@ -71,42 +58,35 @@ contract GasContract {
     }
 
     function getPayments(address _user) public view returns (Payment[] memory payments_) {
-        if (_user == address(0)) revert();
         return payments[_user];
     }
 
     function transfer(address _recipient, uint256 _amount, string calldata _name) public {
-        if (balances[msg.sender] <= _amount && bytes(_name).length > 9) revert();
+        if (bytes(_name).length > 9) revert();
         balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
-        
-        emit Transfer(_recipient, _amount);
         
         Payment memory payment;
 
         payment.paymentID = ++payment.paymentID;
         payments[msg.sender].push(payment);
+
+        emit Transfer(_recipient, _amount);
     }
 
-    function updatePayment(address _user, uint256 _ID, uint256 _amount, PaymentType _type) public onlyAdminOrOwner {
-        if (_ID < 0 && _amount == 0 && _user == address(0)) revert();
+    function updatePayment(address _user, uint256 _ID, uint256 _amount, uint256 _type) public onlyAdminOrOwner {
         for (uint256 ii = 0; ii < payments[_user].length; ii++) {
-            if (payments[_user][ii].paymentID == _ID) {
-                payments[_user][ii].paymentType = _type;
-                payments[_user][ii].amount = _amount;
-            }
+            payments[_user][ii].paymentID == _ID;
+            payments[_user][ii].paymentType = _type;
+            payments[_user][ii].amount = _amount;
         }
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier) public {
-        if (_tier > 255) revert();
         whitelist[_userAddrs] = _tier;
     }
 
-    function whiteTransfer(address _recipient, uint256 _amount, ImportantStruct memory _struct) public {
-        uint256 usersTier = whitelist[msg.sender];
-        
-        if (usersTier < 0 && usersTier > 4 && balances[msg.sender] < _amount && _amount < 3) revert();
+    function whiteTransfer(address _recipient, uint256 _amount, ImportantStruct calldata _struct) public {
         balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
         balances[msg.sender] += whitelist[msg.sender];
@@ -114,8 +94,6 @@ contract GasContract {
 
         ImportantStruct memory newImportantStruct;
 
-        newImportantStruct.valueA = _struct.valueA;
         newImportantStruct.bigValue = _struct.bigValue;
-        newImportantStruct.valueB = _struct.valueB;
     }
 }
